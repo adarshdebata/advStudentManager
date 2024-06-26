50db7cbe26da1e6e4f64b5ea3d82065928d113ef
adarshdebata <adarshdebata00@gmail.com>
2024-06-26 13:02:57 +0530

    db.js modified

--- models/db.js
+++ models/db.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const pool = new Pool({
-    connectionString: "postgresql://Student-management-system_owner:jNo9xis7DkUc@ep-sweet-math-a1pjc4md.ap-southeast-1.aws.neon.tech/Student-management-system?sslmode=require"
+    connectionString: process.env.POSTGRES_STRING
 });
 
 module.exports = pool;
