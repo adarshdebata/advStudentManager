2b0ebcfdeb5eb617783ff8adce5bc36466193ad3
adarshdebata <adarshdebata00@gmail.com>
2024-06-26 11:06:10 +0530

    First Commit

---
+++ model/db.js
@@ -0,0 +1,9 @@
+const Pool = require("pg").Pool;
+
+
+const pool = new Pool({
+    connectionString: "postgresql://Student-management-system_owner:jNo9xis7DkUc@ep-sweet-math-a1pjc4md.ap-southeast-1.aws.neon.tech/Student-management-system?sslmode=require"
+});
+
+
+module.exports = pool;
