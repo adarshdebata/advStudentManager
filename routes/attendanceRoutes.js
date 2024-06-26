d857ac12a3e57ec1c405413330cff43a3288597d
adarshdebata <adarshdebata00@gmail.com>
2024-06-26 17:15:26 +0530

    Attendance Route Update

--- routes/attendanceRoutes.js
+++ routes/attendanceRoutes.js
@@ -8,4 +8,7 @@ router.post('/attendance', attendanceController.addAttendance);
 // GET API 
 router.get('/attendance/:rollno', attendanceController.getPresentTimeForMonth);
 
+// GET API to fetch attendance records for a date range for a particular roll number
+router.get('/attendance/range/:rollno', attendanceController.getAttendanceByDateRange);
+
 module.exports = router;
