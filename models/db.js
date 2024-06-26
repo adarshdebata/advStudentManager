02d660ab9b0da348b7c753cc76a806e1a52f6342
adarshdebata <adarshdebata00@gmail.com>
2024-06-26 11:57:00 +0530

    db pooling modified

--- models/db.js
+++ models/db.js
@@ -1,9 +1,7 @@
-const Pool = require("pg").Pool;
-
+const { Pool } = require('pg');
 
 const pool = new Pool({
     connectionString: "postgresql://Student-management-system_owner:jNo9xis7DkUc@ep-sweet-math-a1pjc4md.ap-southeast-1.aws.neon.tech/Student-management-system?sslmode=require"
 });
-
 
 module.exports = pool;
